930ff9684d70a3e548eed69fb0910c157f3ee389
Aytan Novruzlu <aytannovruzlu@Aytans-Air-4.bbrouter>
2024-01-15 18:48:46 +0400

    update

--- Final/js/homePage.js
+++ Final/js/homePage.js
@@ -3,60 +3,60 @@ let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+	showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+	showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex - 1].style.display = "flex";
+	let i;
+	let slides = document.getElementsByClassName("mySlides");
+	if (n > slides.length) { slideIndex = 1 }
+	if (n < 1) { slideIndex = slides.length }
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	slides[slideIndex - 1].style.display = "flex";
 }
 
 
 
 //countdown
 function uiCountdown(node, locale = 'en-US') {
-    const rtf = new Intl.RelativeTimeFormat(locale, { numeric: "auto" });
-    const setDelay = (name, delay) => {
-        node.style.setProperty(`--delay-${name}`, `${delay}s`);
-    }
-    const setLabel = (type) => {
-        const parts = rtf.formatToParts(3, type)
-        return parts.at(parts.length - 1)?.value
-    }
+	const rtf = new Intl.RelativeTimeFormat(locale, { numeric: "auto" });
+	const setDelay = (name, delay) => {
+		node.style.setProperty(`--delay-${name}`, `${delay}s`);
+	}
+	const setLabel = (type) => {
+		const parts = rtf.formatToParts(3, type)
+		return parts.at(parts.length - 1)?.value
+	}
 
-    const end = new Date(node.getAttribute('data-time')).getTime();
-    const remaining = end - Date.now();
+	const end = new Date(node.getAttribute('data-time')).getTime();
+	const remaining = end - Date.now();
 
-    const DAY = 86400;
-    const HOUR = 3600;
+	const DAY = 86400;
+	const HOUR = 3600;
 
-    const seconds = Math.floor((remaining / 1000) % 60);
-    const days = Math.floor(remaining / (DAY * 1000));
-    const hours = Math.floor((remaining / (HOUR * 1000)) % 24);
-    const minutes = Math.floor((remaining / (60 * 1000)) % 60);
+	const seconds = Math.floor((remaining / 1000) % 60);
+	const days = Math.floor(remaining / (DAY * 1000));
+	const hours = Math.floor((remaining / (HOUR * 1000)) % 24);
+	const minutes = Math.floor((remaining / (60 * 1000)) % 60);
 
-    const SECONDS = -Math.abs(60 - seconds)
-    const MINUTES = -Math.abs(HOUR - (minutes * 60) - (60 - 1) - SECONDS);
-    const HOURS = -Math.abs(DAY - (hours * HOUR) + MINUTES);
+	const SECONDS = -Math.abs(60 - seconds)
+	const MINUTES = -Math.abs(HOUR - (minutes * 60) - (60 - 1) - SECONDS);
+	const HOURS = -Math.abs(DAY - (hours * HOUR) + MINUTES);
 
-    [...node.children].forEach((child, index) => {
-        child.textContent = setLabel(child.dataset.label)
-    })
+	[...node.children].forEach((child, index) => {
+		child.textContent = setLabel(child.dataset.label)
+	})
 
-    setDelay('days', -Math.abs(31536000 - (days * DAY) + HOURS));
-    setDelay('hours', HOURS);
-    setDelay('minutes', MINUTES);
-    setDelay('seconds', SECONDS);
+	setDelay('days', -Math.abs(31536000 - (days * DAY) + HOURS));
+	setDelay('hours', HOURS);
+	setDelay('minutes', MINUTES);
+	setDelay('seconds', SECONDS);
 }
 /* Init, play with `locale`! */
 uiCountdown(document.querySelector('.ui-countdown'), 'en-US')
@@ -157,3 +157,12 @@ document.addEventListener("mouseup", dragStop);
 carousel.addEventListener("scroll", infiniteScroll);
 wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
 wrapper.addEventListener("mouseleave", autoPlay);
+
+
+function showInfo() {
+	document.getElementById('info').style.display = 'none';
+}
+
+function hideInfo() {
+	document.getElementById('info').style.display = 'block';
+}
